67f24a8b4af847179b9303adffc76e9c6b3a64d4
unknown <cogdotnet12@iiht.tech>
2022-05-11 02:10:26 +0530

    Rabbitmq

---
+++ FlightBookingService/UserService/Controllers/RabbitMqController.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.ViewModel;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RabbitMqController : ControllerBase
+    {
+        IPublishEndpoint _ipublishEndpoint;
+
+        public RabbitMqController(IPublishEndpoint ipublishEndpoint)
+        {
+            _ipublishEndpoint = ipublishEndpoint;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(Order order)
+        {
+            await _ipublishEndpoint.Publish<Order>(order);
+            return Ok();
+        }
+
+
+    }
+}

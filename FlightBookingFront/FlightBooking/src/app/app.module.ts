b6dd165a2a167a0b11af6f569f65fe652d8ecd30
ParmarM2809 <parmarmital36@gmail.com>
2022-05-12 16:31:22 +0530

    Components

--- FlightBookingFront/FlightBooking/src/app/app.module.ts
+++ FlightBookingFront/FlightBooking/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FlightListComponent } from './flight-list/flight-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FlightListComponent
   ],
   imports: [
     BrowserModule,
